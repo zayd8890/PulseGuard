121a1b58642ad78574f9543147f2e3e297043738
Zayd <waraglizayd1@gmail.com>
2025-05-02 16:03:07 +0100

    shut down

---
+++ tet.py
@@ -0,0 +1,20 @@
+import matplotlib
+import plotly
+import pymongo
+import dotenv
+import pandas
+import numpy
+import toml
+import altair
+import streamlit
+
+# Print versions
+print(f"Streamlit: {streamlit.__version__}")
+print(f"Matplotlib: {matplotlib.__version__}")
+print(f"Plotly: {plotly.__version__}")
+print(f"PyMongo: {pymongo.__version__}")
+
+print(f"Pandas: {pandas.__version__}")
+print(f"NumPy: {numpy.__version__}")
+print(f"TOML: {toml.__version__}")
+print(f"Altair: {altair.__version__}")
